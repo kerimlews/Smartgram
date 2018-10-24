f0d457b2b55cf4c939ed5e1f6d25f99a65e2c146
Kerim <kerimalihodza22@gmail.com>
2018-10-25 00:05:22 +0200

    saadsa

--- app/containers/home.js
+++ app/containers/home.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
+import { ApolloConsumer } from 'react-apollo';
+import Button from 'components/Button';
+import style from './styles/login';
+
+function signOut(writeData) {
+    try {
+        AsyncStorage.setItem('token', null)
+        writeData({ data: { isAuth: false } });
+    } catch(ex) {
+        console.log('Error sign out ', ex);
+    }
+}
 
 const Home = () => (
-    <View>
-        <Text>
-        Home
-        </Text>
-    </View>
+    <ApolloConsumer>
+        {
+            ({ writeData }) => (
+                <View>
+                    <Text>
+                        Home
+                    </Text>
+                    <Button
+                        style={style.loginBtn}
+                        onPress={() => signOut(writeData)}
+                        text="Sign out"
+                    />
+                </View>
+            )
+        }
+    </ApolloConsumer>
 )
 
 export default Home
--- app/containers/login.js
+++ app/containers/login.js
@@ -35,14 +35,16 @@ export default class Login extends Component {
         error: null
     }
 
-    login = async (cache, { data: { login } }) => {
+    login = (cache, { data: { login } }) => this.saveToken(cache, login)
+    registration = (cache, { data: { registration } }) => this.saveToken(cache, registration)
+
+    saveToken = async (cache, data) => {
         try {
-            await AsyncStorage.setItem('token', login.token)
-            cache.writeQuery({ ...login, isAuth: true });
-            console.log('read', cache.readQuery({ query: gql`{ isAuth @client }` }))
+            await AsyncStorage.setItem('token', data.token)
+            cache.writeData({ data: { ...data, isAuth: true } });
         } catch(ex) {
             this.setState({ error: {
-                graphQLError: [{ message: 'An unexpected error try restart application' }]
+                graphQLErrors: [{ message: 'An unexpected error try restart application' }]
             } })
         }
     }
@@ -50,21 +52,23 @@ export default class Login extends Component {
     render() {
         const { username, password, email, confirmPassword, error } = this.state;
         return (
-            <Mutation mutation={LOGIN} update={this.login}
-          >
+            <Mutation
+                mutation={LOGIN}
+                update={this.login}
+            >
                 {(login, { data: dataLogin, error: errorLogin, loading: loginLoading }) => (
             <Mutation mutation={REGISTRATION}>
                 {(registration, { data: dataRegist, error: errorRegist, loading: registrerLoading }) => (
             <Mutation mutation={LOGIN}>
-                {(connectWithFacebook, { data: dataFb, error, loading }) => {
+                {(connectWithFacebook, { data: dataFb, error: errorFb, loading: loadingFb }) => {
                     
-                    if (loading || loginLoading || registrerLoading) 
+                    if (loadingFb || loginLoading || registrerLoading) 
                         return <Text>LOADING ...............</Text>
                     return (
 
                     <ImageBackground source={require('../../assets/login.jpg')} style={style.login}>
                         <Error
-                            errors={errorLogin || errorRegist || error}
+                            errors={errorLogin || errorRegist || errorFb || error}
                         />
                         <TextInput
                             style={style.textInput}
